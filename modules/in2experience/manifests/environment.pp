e1899e10e74be29cbfb8d4e47392796fdf491de8
Daniel Zwicker <daniel.zwicker@in2experience.com>
2013-03-17 23:14:22 +0100

    copy from private repo

---
+++ modules/in2experience/manifests/environment.pp
@@ -0,0 +1,18 @@
+class in2experience::environment {
+	include wget
+        include caffeine
+	include fluid
+	include skype 
+	include dropbox
+	include flux
+	include sourcetree
+
+	# from the stable channel
+	include chrome
+	# from the dev channel
+	#include chrome::dev
+	# from the nightly channel
+	#include chrome::canary
+	
+	include java
+ }
